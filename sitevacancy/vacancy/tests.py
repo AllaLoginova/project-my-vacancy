6e4addd43794e37797d486dc5029f67ff5996cd9
AllaLoginova <logal2014.mail.ru>
2025-03-24 21:16:40 +0500

    add new file

--- sitevacancy/vacancy/tests.py
+++ sitevacancy/vacancy/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class GetPagesTestCase(TestCase):
+    def setUp(self):
+        "Инициализация перед выполнением каждого теста"
+
+    def test_mainpage(self):
+        path = reverse('home')
+        response = self.client.get(path)
+        print(response)
+
+    def test_case_2(self):
+        pass
+
+    def tearDown(self):
+        "Действие после выполнения каждого теста"
